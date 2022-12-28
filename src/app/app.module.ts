960632d59747be535bc2e060f82fe2ab5fd17322
selvasekhar <selvasekhar@hotmail.com>
2022-12-28 19:45:12 +0530

    Import

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClient, HttpClientModule} from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactAppComponent } from './components/contact-app/contact-app.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { ContactCardComponent } from './components/contact-card/contact-card.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactAppComponent,
+    ContactListComponent,
+    ContactCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
